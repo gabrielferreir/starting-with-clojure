b41d0741b00a234d6bc9e922da336981ca24856e
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-10-14 20:13:10 -0300

    Finalizando jogo da forca

--- src/starting_with_clojure/core.clj
+++ src/starting_with_clojure/core.clj
@@ -1,22 +1,59 @@
 (ns starting-with-clojure.core
   (:gen-class))
 
-(def total-de-vidas 6)
+(def total-de-vidas 3)
+(def palavra-secreta "SANTOS")
+(defn perdeu [] (println "Você perdeu"))
+(defn ganhou [] (println "Você ganhou"))
 
-(defn perdeu [] (print "Você perdeu"))
-(defn perdeu [] (print "Você ganhou"))
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra)
+  )
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos))
+  )
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra]
+  (.contains palavra chute)
+  )
+
+(defn imprime [vidas palavra acertos]
+  (println "Vidas: " vidas)
+  ;(println "Palavra: " palavra)
+  ;(println "Palavra: " palavra
+  (doseq [letra (seq palavra)]
+    (if (contains? acertos (str letra))
+      (print letra " ")
+      (print "_" " ")
+      )
+    )
+  (println))
 
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-        (ganhou)
-        (print "Chute uma palavra")
+  (imprime vidas palavra acertos)
+  (cond
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+    :else
+    (let [chute (le-letra!)]
+      (if (acertou? chute palavra)
+        (do
+          (println "Você acertou essa letra")
+          (recur vidas palavra (conj acertos chute)))
+        (do
+          (println "Você errou essa letra")
+          (recur (dec vidas) palavra acertos))
+        )
+      )
     )
   )
-)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn comeca []
+  (jogo total-de-vidas palavra-secreta #{})
+  )
+
+(defn -main [& args]
+  (comeca))
